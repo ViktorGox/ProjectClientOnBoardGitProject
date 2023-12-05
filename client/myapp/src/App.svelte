c9345601e716794865007bef4f48e6efa5062c69
ViktorKrGo <viktor.krastev03@gmail.com>
2023-12-05 12:47:47 +0100

    Added the page npm package which will allow us to change visible page component. Added a second page just for testing, and a header component.

--- client/myapp/src/App.svelte
+++ client/myapp/src/App.svelte
@@ -1,47 +1,46 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import router from 'page';
+  import TestCaseOverview from './pages/TestCaseOverview.svelte'
+  import SecondPageForTesting from './pages/SecondPageForTesting.svelte'
+  import Header from "./components/Header.svelte";
+
+  let page;
+  let params;
+  let currentRoute;
+
+  router('/', (ctx) => {
+    page = TestCaseOverview;
+    currentRoute = ctx.pathname;
+  });
+  router('/about', (ctx) => {
+    page = SecondPageForTesting;
+    currentRoute = ctx.pathname;
+  });
+
+  router.start();
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <Header active={currentRoute} />
+  <svelte:component this={page} {params} />
 </main>
 
+
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+
+  img {
+    height: 16rem;
+    width: 16rem;
   }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
