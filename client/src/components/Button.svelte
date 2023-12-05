f7b67eb8ac648048473d5d9412b0469e30fa89ac
ViktorKrGo <viktor.krastev03@gmail.com>
2023-12-05 18:57:34 +0100

    Made a svelte component for button. Has some input to make it different scales. Idea is to follow the same design.

---
+++ client/src/components/Button.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let onClick;
+    export let text = "Button";
+    if(!onClick) text = "Unassigned " + text; // no idea why I'm adding this, seems smart tho
+
+    export let width = 20;
+    export let height = 30;
+    export let sizeUnit = "px";
+</script>
+
+<div class="background">
+    <div class="button" on:click={onClick} style="width: {width}{sizeUnit}; height: {height}{sizeUnit}">{text}</div>
+</div>
+
+<style>
+    .button {
+        cursor: pointer;
+        background-color: #B7C100;
+        border-radius: 5px;
+        border: black solid 2px;
+        padding: 7px 20px 7px 20px;
+    }
+</style>
