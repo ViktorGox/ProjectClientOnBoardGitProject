baea5c348a78513de8f370204e575373da7183e6
Justin <497905@student.saxion.nl>
2023-12-06 09:16:38 +0100

    Added connection of database in backend

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mysql from 'mysql'
 const app = express()
 const port = 3000
 
@@ -11,6 +12,17 @@ app.get('/', (req, res) => {
   // res.send('Hello World!')
   res.json({ msg: "hello world"});
 })
+//todo: implement .ENV for security reasons.
+export  let con = mysql.createConnection({
+  host: "database.discordbothosting.com",
+  user: "u2669_s4z6JYe2Hp",
+  password: "j!A!cck!kZZ6Wv7U^G4OTda7"
+});
+//connection to DB
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Database Connected");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
