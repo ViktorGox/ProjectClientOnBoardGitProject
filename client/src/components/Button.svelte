a2cc660e6b943376113e5c4ea41bc8c74cbab7a7
Jafar <alirahmi@gmail.com>
2023-12-06 17:35:19 +0100

    made button component to automatically adjust its size based on its content

--- client/src/components/Button.svelte
+++ client/src/components/Button.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
     export let onClick;
     export let text = "Button";
-    export let width = 50;
-    export let height = 20;
-    export let sizeUnit = "px";
     export let padding;
     export let margin;
 </script>
 
 <div class="background">
-    <div class="button" on:click={onClick} style="margin: {margin}; padding: {padding}; width: {width}{sizeUnit}; height: {height}{sizeUnit}">{text}</div>
+    <div class="button" on:click={onClick} style="margin: {margin}; padding: {padding};">{text}</div>
 </div>
 
 <style>
@@ -18,7 +15,6 @@
         background-color: #B7C100;
         border-radius: 5px;
         border: black solid 2px;
-        padding: 7px 20px 7px 20px;
         align-items: center;
         display: flex;
         justify-content: center;
