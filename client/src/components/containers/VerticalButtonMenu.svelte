cf65bbb62f05e8d9f96e2c8fcb983e0f0845a897
ViktorKrGo <viktor.krastev03@gmail.com>
2023-12-06 23:10:17 +0100

    F-02 #97 Added VerticalButtonMenu.svelte. This item can be given a list of functions, it will then create a button for each using a PresetList.

---
+++ client/src/components/containers/VerticalButtonMenu.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import PresetList from "../List/PresetList.svelte";
+    import SideMenuButton from "../SideMenuButton.svelte";
+
+    export let name = "Button Menu";
+    export let buttonOnClicks = [];
+</script>
+
+<div class="background">
+    <div class="top">
+        {name}
+    </div>
+    <div class="buttons">
+        <PresetList itemList={buttonOnClicks} noItemsText="No items found to show.">
+            <div slot="header">
+            </div>
+            <div slot="item" let:item let:index>
+                <SideMenuButton onClick={buttonOnClicks[index]}></SideMenuButton>
+            </div>
+        </PresetList>
+    </div>
+</div>
+
+<style>
+    .background {
+        background-color: rgba(255, 255, 255, 0.5);
+    }
+</style>
