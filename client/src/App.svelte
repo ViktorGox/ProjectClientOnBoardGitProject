189fb7721972774ddd861bdcac928002e272120d
ViktorKrGo <viktor.krastev03@gmail.com>
2023-12-06 17:32:36 +0100

    F-02 #97 Added the sidebar component, currently is empty. Added it so that I can implement it into the App.svelte. Will currently appear onto log in page but will be fixed when the log in page is added.

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,46 +1,59 @@
 <script lang="ts">
-  import router from 'page';
-  import TestCaseOverview from './pages/TestCaseOverview.svelte'
-  import SecondPageForTesting from './pages/SecondPageForTesting.svelte'
-  import Header from "./components/Header.svelte";
+    import router from 'page';
+    import TestCaseOverview from './pages/TestCaseOverview.svelte'
+    import SecondPageForTesting from './pages/SecondPageForTesting.svelte'
+    import Header from "./components/Header.svelte";
+    import Sidebar from "./components/Sidebar.svelte";
 
-  let page;
-  let params;
-  let currentRoute;
+    let page;
+    let params;
+    let currentRoute;
 
-  router('/', (ctx) => {
-    page = TestCaseOverview;
-    currentRoute = ctx.pathname;
-  });
-  router('/about', (ctx) => {
-    page = SecondPageForTesting;
-    currentRoute = ctx.pathname;
-  });
+    router('/', (ctx) => {
+        page = TestCaseOverview;
+        currentRoute = ctx.pathname;
+    });
+    router('/about', (ctx) => {
+        page = SecondPageForTesting;
+        currentRoute = ctx.pathname;
+    });
 
-  router.start();
+    router.start();
 </script>
 
 <main>
-  <Header active={currentRoute} />
-  <svelte:component this={page} {params} />
+    <Header active={currentRoute}/>
+    <div class="main">
+        <Sidebar></Sidebar>
+        <div class="page">
+            <svelte:component this={page} {params}/>
+        </div>
+    </div>
 </main>
 
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
+    :root {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+        Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
+    main {
+        text-align: center;
+        padding: 1em;
+        margin: 0 auto;
+    }
 
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
+    img {
+        height: 16rem;
+        width: 16rem;
+    }
 
+    .main {
+        display: flex;
+    }
+
+    .page {
+        width: 85%;
+    }
 </style>
