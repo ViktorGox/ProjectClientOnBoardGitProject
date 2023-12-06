56541a554d4bd71ce334511debac87ed1824a109
ViktorKrGo <viktor.krastev03@gmail.com>
2023-12-06 22:51:47 +0100

    F-02 #97 Added SideMenuButton.svelte. Requires different parameters from the Button.svelte, so I created a new one.

---
+++ client/src/components/SideMenuButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let onClick;
+    export let text = "SideButton";
+</script>
+
+<div class="background">
+    <div class="button" on:click={onClick}>{text}</div>
+</div>
+
+<style>
+    .background {
+        background-color: rgba(255,255,255,0.5);
+        width: 100%;
+        height: 30px;
+        display: flex;
+        padding: 0 0 0 25px;
+        align-items: center;
+        /*border-bottom: 2px solid white;*/
+    }
+</style>
