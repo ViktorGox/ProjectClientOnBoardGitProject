b79aed13f12810ad568342bee1e93f46fd53111f
Jafar <alirahmi@gmail.com>
2023-12-07 16:29:35 +0100

    Added new app.svelte

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,48 +1,62 @@
-<script>
-  import logo from './assets/svelte.png'
-  import router from 'page';
+<script lang="ts">
+    import router from 'page';
+    import TestCaseOverview from './pages/TestCaseOverview.svelte'
+    import SecondPageForTesting from './pages/SecondPageForTesting.svelte'
+    import Header from "./components/Header.svelte";
 
-  import Home from "./pages/Home.svelte";
-  import About from "./pages/About.svelte";
-  import Header from "./components/Header.svelte";
+    import Sidebar from "./components/Sidebar.svelte";
+    import Projects from "./pages/Projects.svelte";
 
-  let page;
-  let params;
-  let currentRoute;
+    let page;
+    let params;
+    let currentRoute;
 
-  router('/', (ctx) => {
-    page = Home;
-    currentRoute = ctx.pathname;
-  });
-  router('/about', (ctx) => {
-    page = About;
-    currentRoute = ctx.pathname;
-  });
+    router('/', (ctx) => {
+        page = TestCaseOverview;
+        currentRoute = ctx.pathname;
+    });
+    router('/projects', (ctx) => {
+        page = Projects;
+        currentRoute = ctx.pathname;
+    });
 
-  router.start();
+    router.start();
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <Header active={currentRoute} />
-  <svelte:component this={page} {params} />
+    <Header active={currentRoute}/>
+    <div class="main">
+        <Sidebar></Sidebar>
+        <div class="page">
+            <svelte:component this={page} {params}/>
+        </div>
+    </div>
 </main>
 
+
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
+    @import "styles/style.css";
+    :root {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+        Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
+    main {
+        text-align: center;
+        padding: 1em;
+        margin: 0 auto;
+    }
 
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
+    img {
+        height: 16rem;
+        width: 16rem;
+    }
 
+    .main {
+        display: flex;
+    }
+
+    .page {
+        width: 85%;
+    }
 </style>
