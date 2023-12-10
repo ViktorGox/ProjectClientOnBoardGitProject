62997da5dda8c9e5e7b1039c6ea2652bfa87c58b
ViktorKrGo <viktor.krastev03@gmail.com>
2023-12-10 12:14:14 +0100

    F-02 #97 Mostly finished side menu. Background color will probably be changed, but I will keep it purple for the moment. Can edit links at later point.

--- client/src/components/Sidebar.svelte
+++ client/src/components/Sidebar.svelte
@@ -1,17 +1,25 @@
 <script>
     import SideMenuButton from "./SideMenuButton.svelte";
+    import VerticalButtonMenu from "./containers/VerticalButtonMenu.svelte";
+
+    const hrefsTestCases = ["/first","/second"]
+    const titlesTestCases = ["first","second"]
+
+    const hrefsAnalytics = ["/third","/fourth"]
+    const titlesAnalytics = ["third","fourth"]
 </script>
 
 <div class="background">
-    <SideMenuButton text="Dashboard"></SideMenuButton>
-    <SideMenuButton text="Projects"></SideMenuButton>
-
+    <SideMenuButton text="Dashboard" href="/"></SideMenuButton>
+    <SideMenuButton text="Projects" href="projects"></SideMenuButton>
+    <SideMenuButton text="Projects" href="projects"></SideMenuButton>
+    <VerticalButtonMenu name="Test Cases" links={hrefsTestCases} titles={titlesTestCases}></VerticalButtonMenu>
+    <VerticalButtonMenu name="Analytics" links={hrefsAnalytics} titles={titlesAnalytics}></VerticalButtonMenu>
 </div>
 
 <style>
     .background {
         background: #381D48;
         width: 250px;
-        color:white;
     }
 </style>
