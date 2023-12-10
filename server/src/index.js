f583f8613cfa159408783a3f529c32ada274068b
Ferhat Kelten <ferhatkelten@gmail.com>
2023-12-10 22:51:07 +0100

    backend-> token generation is successful

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,20 @@
-import dotenv from 'dotenv';
+import express from "express";
+import cors from 'cors';
+
+import * as database from "./database/database.js";
+import tokenRouter from "./routes/token-router.js";
+
+const port = 3000;
+const app = express();
+
+database.createDatabase();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/token', tokenRouter);
 
 
-dotenv.config()
+app.listen(port, function () {
+    console.log(`Server listening on port ${port}`);
+});
