be0ed90b9a3f211cdee1c5157d1fa7cc02739338
ViktorKrGo <viktor.krastev03@gmail.com>
2023-12-10 19:05:42 +0100

    F-02 #97 Added the option to add an image in front of the link.

--- client/src/components/SideMenuButton.svelte
+++ client/src/components/SideMenuButton.svelte
@@ -1,9 +1,14 @@
 <script>
     export let text = "SideButton";
     export let href;
+    export let image;
 </script>
 
 <a class="background" href={href}>
+    <img src={image}>
+    {#if !image}
+        <div class="button" style="margin-left: 15px">{text}</div>
+    {/if}
     <div class="button">{text}</div>
 </a>
 
@@ -16,5 +21,9 @@
         align-items: center;
         text-decoration: none;
         color: white;
+    }
+
+    img {
+        height: 15px;
     }
 </style>
--- client/src/components/Sidebar.svelte
+++ client/src/components/Sidebar.svelte
@@ -7,14 +7,18 @@
 
     const hrefsAnalytics = ["/third","/fourth"]
     const titlesAnalytics = ["third","fourth"]
+
+    const imagesAll = ["./src/assets/svelte.png","./src/assets/svelte.png"]
+    const imagesHalf = ["","./src/assets/svelte.png"]
 </script>
 
 <div class="background">
-    <SideMenuButton text="Dashboard" href="/"></SideMenuButton>
-    <SideMenuButton text="Projects" href="projects"></SideMenuButton>
+    <SideMenuButton text="Dashboard" href="/" image="./src/assets/svelte.png"></SideMenuButton>
+    <SideMenuButton text="Projects" href="projects" image="./src/assets/svelte.png"></SideMenuButton>
     <SideMenuButton text="Projects" href="projects"></SideMenuButton>
     <VerticalButtonMenu name="Test Cases" links={hrefsTestCases} titles={titlesTestCases}></VerticalButtonMenu>
-    <VerticalButtonMenu name="Analytics" links={hrefsAnalytics} titles={titlesAnalytics}></VerticalButtonMenu>
+    <VerticalButtonMenu name="Analytics" links={hrefsAnalytics} titles={titlesAnalytics} images={imagesAll}></VerticalButtonMenu>
+    <VerticalButtonMenu name="ThirdOneForExample" links={hrefsAnalytics} titles={titlesAnalytics} images={imagesHalf}></VerticalButtonMenu>
 </div>
 
 <style>
