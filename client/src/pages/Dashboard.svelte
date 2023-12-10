813e9cd9c3b26c06978e33f59f5c7be91cba7ad8
Jafar <alirahmi@gmail.com>
2023-12-10 15:50:33 +0100

    Edited Dashboard page template

--- client/src/pages/Dashboard.svelte
+++ client/src/pages/Dashboard.svelte
@@ -1,101 +1,38 @@
 <script>
 
+        let dashboardBoxes = [
+        {icon: 'bi bi-clock text-primary', title: 'Total Time Testing', value: 0},
+        {icon: 'bi bi-clock text-success', title: 'Average time per test', value: 0},
+        {icon: 'bi bi-list-ul text-primary', title: 'Total test cases', value: 0},
+        {icon: 'bi bi-code-square text-info', title: 'Not sure for here yet', value: 0},
+        {icon: 'bi bi-check-square text-success', title: 'Passed Total', value: 0},
+        {icon: 'bi bi-x-circle text-danger', title: 'Failed Total', value: 0},
+        {icon: 'bi bi-exclamation-triangle text-warning', title: 'Bugs', value: 0},
+        {icon: 'bi bi-dash-circle text-danger', title: 'Blockers', value: 0},
+        ];
+
 </script>
-<main >
-    <div class="dash">
-        <div class="container px-4 py-5" id="icon-grid">
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
-                <div class="col d-flex align-items-start">
-                    <div class="row dash-box">
-                        <div class="col-4">
-                            <i class="bi bi-clock text-primary" style="font-size: 60px;"></i>
+<main>
+
+        <div class="dash">
+            <div class="container px-4 py-5" id="icon-grid">
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
+                    {#each dashboardBoxes as box (box.title)}
+                        <div class="col d-flex align-items-start">
+                            <div class="row dash-box">
+                                <div class="col-4">
+                                    <i class={box.icon} style="font-size: 60px;"></i>
+                                </div>
+                                <div class="col-8">
+                                    <p class="fw-bold mb-0 fs-4">{box.value} seconds</p>
+                                    <p>{box.title}</p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-8">
-                            <p class="fw-bold mb-0 fs-4">0 seconds</p>
-                            <p>Total Time Testing</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col d-flex align-items-start">
-                    <div class="row dash-box">
-                        <div class="col-4">
-                            <i class="bi bi-clock text-success" style="font-size: 60px;"></i>
-                        </div>
-                        <div class="col-8">
-                            <p class="fw-bold mb-0 fs-4">0 seconds</p>
-                            <p>Average time per test</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col d-flex align-items-start">
-                    <div class="row dash-box">
-                        <div class="col-4">
-                            <i class="bi bi-list-ul text-primary" style="font-size: 60px;"></i>
-                        </div>
-                        <div class="col-8">
-                            <p class="fw-bold mb-0 fs-4">0 seconds</p>
-                            <p>Total test cases</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col d-flex align-items-start">
-                    <div class="row dash-box">
-                        <div class="col-4">
-                            <i class="bi bi-code-square text-info " style="font-size: 60px;"></i>
-                        </div>
-                        <div class="col-8">
-                            <p class="fw-bold mb-0 fs-4">0 seconds</p>
-                            <p>Not sure for here yet</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col d-flex align-items-start">
-                    <div class="row dash-box">
-                        <div class="col-4">
-                            <i class="bi bi-check-square text-success" style="font-size: 60px;"></i>
-                        </div>
-                        <div class="col-8">
-                            <p class="fw-bold mb-0 fs-4">0 seconds</p>
-                            <p>Passed Total</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col d-flex align-items-start">
-                    <div class="row dash-box">
-                        <div class="col-4">
-                            <i class="bi bi-x-circle text-danger" style="font-size: 60px;"></i>
-                        </div>
-                        <div class="col-8">
-                            <p class="fw-bold mb-0 fs-4">0 seconds</p>
-                            <p>Failed Total</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col d-flex align-items-start">
-                    <div class="row dash-box">
-                        <div class="col-4">
-                            <i class="bi bi-exclamation-triangle text-warning " style="font-size: 60px;"></i>
-                        </div>
-                        <div class="col-8">
-                            <p class="fw-bold mb-0 fs-4">0 seconds</p>
-                            <p>Bugs</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col d-flex align-items-start">
-                    <div class="row dash-box">
-                        <div class="col-4">
-                            <i class="bi bi-dash-circle text-danger" style="font-size: 60px;"></i>
-                        </div>
-                        <div class="col-8">
-                            <p class="fw-bold mb-0 fs-4">0 seconds</p>
-                            <p>Blockers</p>
-                        </div>
-                    </div>
+                    {/each}
                 </div>
             </div>
         </div>
-    </div>
     <section class="graph">
 
     </section>
