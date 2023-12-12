1b16c8da11831e10b75a1aaf32a44f5d8fd8b98f
ViktorKrGo <viktor.krastev03@gmail.com>
2023-12-12 09:06:15 +0100

    #9 Somehow sidebar update was deleted, so I am recommitting this.

--- client/src/components/Sidebar.svelte
+++ client/src/components/Sidebar.svelte
@@ -2,19 +2,23 @@
     import SideMenuButton from "./SideMenuButton.svelte";
     import VerticalButtonMenu from "./containers/VerticalButtonMenu.svelte";
 
-    const hrefsTestCases = ["/","/second"]
+    const hrefsTestCases = ["/first","/second"]
     const titlesTestCases = ["first","second"]
 
     const hrefsAnalytics = ["/third","/fourth"]
     const titlesAnalytics = ["third","fourth"]
+
+    const imagesAll = ["./src/assets/svelte.png","./src/assets/svelte.png"]
+    const imagesHalf = ["","./src/assets/svelte.png"]
 </script>
 
 <div class="background">
-    <SideMenuButton text="Dashboard" href="/projects/:id"></SideMenuButton>
+    <SideMenuButton text="Dashboard" href="/" image="./src/assets/svelte.png"></SideMenuButton>
+    <SideMenuButton text="Projects" href="projects" image="./src/assets/svelte.png"></SideMenuButton>
     <SideMenuButton text="Projects" href="projects"></SideMenuButton>
-    <SideMenuButton text="Reports" href="projects"></SideMenuButton>
     <VerticalButtonMenu name="Test Cases" links={hrefsTestCases} titles={titlesTestCases}></VerticalButtonMenu>
-    <VerticalButtonMenu name="Settings" links={hrefsAnalytics} titles={titlesAnalytics}></VerticalButtonMenu>
+    <VerticalButtonMenu name="Analytics" links={hrefsAnalytics} titles={titlesAnalytics} images={imagesAll}></VerticalButtonMenu>
+    <VerticalButtonMenu name="ThirdOneForExample" links={hrefsAnalytics} titles={titlesAnalytics} images={imagesHalf}></VerticalButtonMenu>
 </div>
 
 <style>
