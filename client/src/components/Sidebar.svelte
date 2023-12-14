b02643ba96a8a149f32e2d9f6e59ac57dcbf6889
Justin <497905@student.saxion.nl>
2023-12-14 14:32:16 +0100

    Header styled and added to make it dissapear when the user is not logged in

--- client/src/components/Sidebar.svelte
+++ client/src/components/Sidebar.svelte
@@ -2,29 +2,40 @@
     import SideMenuButton from "./SideMenuButton.svelte";
     import VerticalButtonMenu from "./containers/VerticalButtonMenu.svelte";
 
-    const hrefsTestCases = ["/first","/second"]
-    const titlesTestCases = ["first","second"]
+    const hrefsTestCases = ["/first", "/second"];
+    const titlesTestCases = ["First", "Second"];
 
-    const hrefsAnalytics = ["/third","/fourth"]
-    const titlesAnalytics = ["third","fourth"]
+    const hrefsAnalytics = ["/third", "/fourth"];
+    const titlesAnalytics = ["Third", "Fourth"];
 
-    const imagesAll = ["./src/assets/svelte.png","./src/assets/svelte.png"]
-    const imagesHalf = ["","./src/assets/svelte.png"]
+    const imagesAll = ["./src/assets/svelte.png", "./src/assets/svelte.png"];
+    const imagesHalf = ["", "./src/assets/svelte.png"];
 </script>
 
 <div class="background">
-    <SideMenuButton text="Dashboard" href="/" image="./src/assets/svelte.png"></SideMenuButton>
-    <SideMenuButton text="Projects" href="projects" image="./src/assets/svelte.png"></SideMenuButton>
-    <SideMenuButton text="Projects" href="projects"></SideMenuButton>
+    <div class="menu">
+        <SideMenuButton text="Dashboard" href="/" image="./src/assets/svelte.png"></SideMenuButton>
+        <SideMenuButton text="Projects" href="projects" image="./src/assets/svelte.png"></SideMenuButton>
+        <SideMenuButton text="Projects" href="projects" image="./src/assets/svelte.png"></SideMenuButton>
+    </div>
+
     <VerticalButtonMenu name="Test Cases" links={hrefsTestCases} titles={titlesTestCases}></VerticalButtonMenu>
     <VerticalButtonMenu name="Analytics" links={hrefsAnalytics} titles={titlesAnalytics} images={imagesAll}></VerticalButtonMenu>
-    <VerticalButtonMenu name="ThirdOneForExample" links={hrefsAnalytics} titles={titlesAnalytics} images={imagesHalf}></VerticalButtonMenu>
+    <VerticalButtonMenu name="Example" links={hrefsAnalytics} titles={titlesAnalytics} images={imagesHalf}></VerticalButtonMenu>
 </div>
 
 <style>
     .background {
-        background: #607D8B;
-        width: 250px;
+        background: #2c3e50;
+        width: 20rem;
         height: 100vh;
+        padding: 2rem;
+        box-sizing: border-box;
+        color: #ecf0f1;
     }
+
+    .menu {
+        margin-bottom: .2rem;
+    }
+
 </style>
