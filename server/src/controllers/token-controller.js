4e5ef8ddb6e5d6090e061c378c83dc092a6b6cb8
Jafar <alirahmi@gmail.com>
2023-12-17 12:33:40 +0100

    Fixed token controller

--- server/src/controllers/token-controller.js
+++ server/src/controllers/token-controller.js
@@ -1,43 +1,54 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import * as userDb from "../database/user-database.js";
+import * as userDb from "../database/database.js";
 
-export function checkToken(req, res) {
+export async function checkToken(req, res) {
     const givenUsername = req.body.username;
     const givenPassword = req.body.password;
 
-    let foundUser = getUser(givenUsername);
+    try {
+        const foundUser = await getUser(givenUsername);
 
-    if (!foundUser) {
-        return res.status(404).json({error: 'Username could not found'})
+        if (!foundUser) {
+            return res.status(404).json({ error: 'Username not found' });
+        }
+
+        const storedPassword = foundUser.password;
+
+        checkPassword(givenPassword, storedPassword, foundUser, res);
+    } catch (error) {
+        console.error('Error checking token:', error);
+        return res.status(500).json({ error: 'Internal server error' });
     }
-
-    const storedPassword = foundUser.password;
-
-    checkPassword(givenPassword, storedPassword, foundUser, res);
 }
 
-function getUser(givenUsername) {
-    const allUsers = userDb.getAllUsers();
+async function getUser(givenUsername) {
+    try {
+        const allUsers = await userDb.getAllUsers();
 
-    let foundUser;
-    for (const user of allUsers) {
-        if (user.username === givenUsername) {
-            foundUser = user;
-            break;
+        let foundUser;
+        for (const user of allUsers) {
+            if (user.username === givenUsername) {
+                foundUser = user;
+                break;
+            }
         }
+
+        return foundUser;
+    } catch (error) {
+        console.error('Error getting user:', error);
+        throw error;
     }
-    return foundUser;
 }
 
 function checkPassword(givenPassword, storedPassword, user, res) {
     bcrypt.compare(givenPassword, storedPassword, function (err, result) {
         if (err) {
-            return res.status(500).json({error: 'Internal server error'});
+            return res.status(500).json({ error: 'Internal server error' });
         } else if (result) {
             createToken(user, res);
         } else {
-            return res.status(401).json({error: 'Invalid password'});
+            return res.status(401).json({ error: 'Invalid password' });
         }
     });
 }
@@ -46,13 +57,12 @@ function createToken(user, res) {
     jwt.sign({
         username: user.username,
         role: user.role
-    }, 'secretKey', {expiresIn: '12h'}, (err, token) => {
+    }, 'secretKey', { expiresIn: '12h' }, (err, token) => {
         if (err) {
-            return res.status(500).json({error: 'Token generation failed'});
+            return res.status(500).json({ error: 'Token generation failed' });
         } else {
             console.log("Token generation is successful");
-            return res.status(200).json({token});
+            return res.status(200).json({ token });
         }
     });
 }
-
