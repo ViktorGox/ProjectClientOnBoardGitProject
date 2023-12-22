162f33b8ace9467adb9d8415cc308da59031c430
ViktorKrGo <viktor.krastev03@gmail.com>
2023-12-22 12:34:43 +0100

    FR-18 #113 Now when the server is restarted, the database is dropped and recreated.

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from 'cors';
+import * as fs from "fs";
 
 import tokenRouter from "./routes/token-router.js";
 import userRouter from "./routes/user-router.js";
 import testsRouter from "./routes/test-router.js";
+import {performQuery} from "./database/database.js";
 
 const port = 3000;
 const app = express();
@@ -14,6 +16,9 @@ app.use(express.json());
 app.use('/token', tokenRouter);
 app.use('/users', userRouter);
 app.use('/test', testsRouter)
+
+const sqlFile = fs.readFileSync('../server/src/database/DatabaseScript.sql', 'utf8');
+performQuery(sqlFile);
 
 app.listen(port, function () {
     console.log(`Server listening on port ${port}`);
