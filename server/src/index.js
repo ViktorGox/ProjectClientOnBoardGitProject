d29e4ed606afccbb5fda7dc351be8715ba808f02
ViktorKrGo <viktor.krastev03@gmail.com>
2024-01-02 21:41:27 +0100

    US-08 BI-1 Added status controller and router. Only contains get.

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,7 @@ import tokenRouter from "./routes/token-router.js";
 import userRouter from "./routes/user-router.js";
 import testsRouter from "./routes/test-router.js";
 import {performQuery} from "./database/database.js";
+import statusRouter from "./routes/status-router.js";
 
 const port = 3000;
 const app = express();
@@ -16,9 +17,10 @@ app.use(express.json());
 app.use('/token', tokenRouter);
 app.use('/users', userRouter);
 app.use('/test', testsRouter)
+app.use('/status', statusRouter);
 
 const sqlFile = fs.readFileSync('../server/src/database/DatabaseScript.sql', 'utf8');
-performQuery(sqlFile);
+void performQuery(sqlFile);
 
 app.listen(port, function () {
     console.log(`Server listening on port ${port}`);
