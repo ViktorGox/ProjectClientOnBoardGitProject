761d0d166ecd8982e61856b5f103434232327704
ViktorKrGo <viktor.krastev03@gmail.com>
2024-01-03 23:02:23 +0100

    US-08 BI-1 Made the page request all tests, modules and status, then convert to correct object.

--- client/src/pages/TestCaseOverview.svelte
+++ client/src/pages/TestCaseOverview.svelte
@@ -1,11 +1,53 @@
-<script lang="ts">
+<script>
     import HorizontalSplitContainer from "../components/containers/HorizontalSplitContainer.svelte";
     import Button from "../components/Button.svelte";
     import TestCaseHorizontal from "../components/containers/TestCaseHorizontal.svelte";
+    import {fetchRequest} from "../lib/Request.js";
 
-    function a() {
 
+    Promise.all([fetchStatuses(), fetchTests(), fetchModules()])
+        .then(([statuses, tests, modules]) => {
+            tests.forEach(async (element) => {
+                element.status = statuses.get(element.statusid);
+                delete element.statusid
+
+                element.modules = [];
+                await fetchRequest('module/' + element.testid).then((result) => {
+                    result.forEach((moduleData) => {
+                        element.modules.push(modules.get(moduleData.moduleid));
+                    });
+                });
+            });
+            console.log(tests);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+
+    function fetchTests() {
+        return fetchRequest('test').then((result) => {
+            return result;
+        }).catch((e) => {
+            throw e;
+        })
     }
+
+    function fetchStatuses() {
+        return fetchRequest('status').then((result) => {
+            return new Map(result.map(status => [status.statusid, status.name]));
+        }).catch((e) => {
+            throw e;
+        })
+    }
+
+    function fetchModules() {
+        return fetchRequest('module').then((result) => {
+            return new Map(result.map(module => [module.moduleid, module.label]));
+        }).catch((e) => {
+            throw e;
+        })
+    }
+
 </script>
 
 <div class="background">
@@ -32,7 +74,8 @@
         <TestCaseHorizontal weight="232"></TestCaseHorizontal>
         <TestCaseHorizontal assignee="SuperVeryLongNameForAnAssignee"></TestCaseHorizontal>
         <TestCaseHorizontal weight="232" assignee="SuperVeryLongNameForAnAssignee"></TestCaseHorizontal>
-        <TestCaseHorizontal module="SomeVeryLongModuleJustABitMore" name="AVeryVerySuperMuchLongNameForATestCaseToTestTheCSSIfItsWorkingCorrectlyAndItSeemsToBeWorkingCorrectly"></TestCaseHorizontal>
+        <TestCaseHorizontal module="SomeVeryLongModuleJustABitMore"
+                            name="AVeryVerySuperMuchLongNameForATestCaseToTestTheCSSIfItsWorkingCorrectlyAndItSeemsToBeWorkingCorrectly"></TestCaseHorizontal>
         <TestCaseHorizontal></TestCaseHorizontal>
         <TestCaseHorizontal></TestCaseHorizontal>
         <TestCaseHorizontal></TestCaseHorizontal>
