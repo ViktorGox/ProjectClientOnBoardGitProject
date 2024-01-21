3f5975dd2c0d3e58fc95695789ec461eadf00f60
ViktorKrGo <viktor.krastev03@gmail.com>
2024-01-21 15:47:49 +0100

    NF-05 Back end is capable of returning sum of weight for a test.

--- server/src/controllers/test-controller.js
+++ server/src/controllers/test-controller.js
@@ -1,4 +1,5 @@
 import {badRequestOnly, notFoundReq} from "./generic.js";
+import {performQuery} from "../database/database.js";
 
 export function getQuery(req, res) {
     return badRequestOnly(req, res);
@@ -22,4 +23,12 @@ export function getTestById(req, res) {
 
 export function putTestById(req, res) {
     return notFoundReq(req, res);
+}
+
+export async function getTotalWeight(req, res) {
+    const sumWeightQuery = 'select sum(weight) from teststep where testid=$1';
+    const weight = await performQuery(sumWeightQuery, [req.params.testid]);
+    console.log(weight);
+    if (weight[0].sum === null) return res.status(404).json({"erorr": "The test was not found."})
+    return res.status(200).json(weight[0]);
 }
