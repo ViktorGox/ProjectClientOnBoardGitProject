595a76dcac8e935cc8e47e8223301d4e918f6b3c
ViktorKrGo <viktor.krastev03@gmail.com>
2024-01-21 11:47:11 +0100

    NF-09 Updating tests status id in testing is now working in the back end.

--- server/src/routes/testing-router.js
+++ server/src/routes/testing-router.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {getAll} from "../controllers/testing-controller.js";
+import {getAll, updateTestStatus} from "../controllers/testing-controller.js";
 
 const router = express.Router();
 
 router.get('/', getAll);
+router.put('/:sprintid/status/:testid', updateTestStatus)
 
 export default router;
